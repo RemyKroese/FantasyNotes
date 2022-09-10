2f028e0f6a1bc681cf6db1a8dbcce341a613a7e2
RemyKroese <rkroese96@gmail.com>
2022-09-10 04:32:54 +0200

    [impl] add campaign save functionality + tests
    * [impl] add campaign save functionality + tests
    
    * [impl] add campaign save functionality + tests
    
    * [fix] failing github workflow
    
    * [fix] failing github workflow
    
    * [fix] failing github workflow

--- backend/notebook.py
+++ backend/notebook.py
@@ -1,3 +1,7 @@
+import os
+import datetime
+import json
+from types import SimpleNamespace
 import backend.models as m
 
 
@@ -13,9 +17,32 @@ class Notebook:
         self.current_campaign = campaign
 
 
-def load_data():
-    return True
+def save_campaign_data(campaign):
+    if not os.path.exists('saves'):
+        os.makedirs('saves')
+    file_path = 'saves/' + str(campaign.name) + '.json'
+    with open(file_path, 'w') as write_file:
+        json.dump(campaign.data(), write_file, cls=ComplexEncoder, indent=4)
+
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, 'data'):
+            return obj.data()
+        elif isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+def load_campaign_data(file_path):
+    with open(file_path, 'r') as file:
+        return json.loads(file, object_hook=lambda data: SimpleNamespace(**data))
 
 
 if __name__ == "__main__":
+    campaign = m.Campaign('campaign_1', notes=[
+        m.Note(text='new note 1', tags=['Tag1', 'Tag2', 'Tag_ALL']),
+        m.Note(text='note 2', tags=['Tag2', 'Tag_ALL'])])
+    save_campaign_data(campaign)
     print('Hello World')
